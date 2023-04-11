be1e5e6cbc8654bafc115f89f99b0029d4b87888
rasoul <r.zamani72@gmail.com>
2023-04-11 14:09:19 +0330

    add login

--- A_BookStore/urls.py
+++ A_BookStore/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # user management
+    path('accounts/', include('django.contrib.auth.urls') ),
+
+    # local apps
     path('', include('pages.urls')),
 ]
